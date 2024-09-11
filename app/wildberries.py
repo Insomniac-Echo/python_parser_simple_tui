84d570a926db1116cc6a511e7069b6673356d067
Insomniac Echo <insomniacecho@gmail.com>
2024-09-11 20:42:58 +0400

    Готовый парсер WB v1.0

--- app/wildberries.py
+++ app/wildberries.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 def get_data():
-    url = 'https://catalog.wb.ru/catalog/men_clothes1/v2/catalog?ab_testing=false&appType=1&cat=8144&curr=rub&dest=-1257786&sort=popular&spp=30'
-    
-    #Для вб эту штуку я бы сделал более гибкой как и юрл обоссаный, это точно нужно доработать, ибо сейчас он по хардкоженой ссылке срёт
+    urlencode = input('Введите запрос поиска (например, "Телефон"): ')
+    url = fr'https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testid=rerank_ksort_promo&appType=1&curr=rub&dest=-284542&query={urlencode}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false'
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0',
         'Accept': '*/*',
@@ -28,6 +28,7 @@ def get_data():
             with open('data.json', 'w', encoding='UTF-8') as file:
                 json.dump(data, file, indent=2, ensure_ascii=False)
                 print(f'Данные сохранены в data.json')
+            return data
         except ValueError:
             print("Ответ не является JSON")
     else:
@@ -35,6 +36,55 @@ def get_data():
         print("Время выполнения запроса:", response.elapsed.total_seconds(), "секунд")
         pass
 
+def get_data_from_json(json_file):
+    data_list = []
+    for data in json_file['data']['products']:
+        id = data.get('id')
+        name = data.get('name')
+        cashback = data.get('feedbackPoints')
+        sale = data.get('sale')
+        brand = data.get('brand')
+        rating = data.get('rating')
+        supplier = data.get('supplier')
+        supplierRating = data.get('supplierRating')
+        feedbacks = data.get('feedbacks')
+        reviewRating = data.get('reviewRating')
+        promoTextCard = data.get('promoTextCard')
+        promoTextCat = data.get('promoTextCat')
+        price_details = data.get('sizes', [{}])[0].get('price', {})
+        basic_price = (price_details.get('basic') / 100)
+        product_price = (price_details.get('product') / 100)
+        total_price = (price_details.get('total') / 100)
+        logistics_price = price_details.get('logistics')
+        return_price = price_details.get('return')
+
+        data_list.append({
+            'id': id,
+            'name': name,
+            'cashback': cashback,
+            'sale': sale,
+            'brand': brand,
+            'rating': rating,
+            'supplier': supplier,
+            'supplierRating': supplierRating,
+            'feedbacks': feedbacks,
+            'reviewRating': reviewRating,
+            'promoTextCard': promoTextCard,
+            'promoTextCat': promoTextCat,
+            'basic_price': basic_price,
+            'product_price': product_price,
+            'total_price': total_price,
+            'logistics_price': logistics_price,
+            'return_price': return_price,
+            'link': f'https://www.wildberries.ru/catalog/{data.get("id")}/detail.aspx?targetUrl=BP'
+        })
+    return data_list
+
+def save_data_to_json(data_list: list, filename: str):
+    with open(filename, 'w', encoding='UTF-8') as file:
+        json.dump(data_list, file, indent=2, ensure_ascii=False)
+    print(f'Форматированные данные сохранены в {filename}')
+
 def wb_parser():
-    #print("wildberries parser placeholder")
-    get_data()
+    export = get_data_from_json(get_data())
+    save_data_to_json(export, 'extracted_data.json')
