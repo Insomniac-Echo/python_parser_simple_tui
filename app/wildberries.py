a3eef78b0a6ecb7a7c30d6e1bf97275c7c378f45
Insomniac Echo <insomniacecho@gmail.com>
2024-09-11 19:05:08 +0400

    WB parser first attempt

--- app/wildberries.py
+++ app/wildberries.py
@@ -1,2 +1,40 @@
+import requests
+import json
+
+def category_get():
+    url = 'https://catalog.wb.ru/catalog/men_clothes1/v2/catalog?ab_testing=false&appType=1&cat=8144&curr=rub&dest=-1257786&sort=popular&spp=30'
+    
+    #Для вб эту штуку я бы сделал более гибкой как и юрл обоссаный, это точно нужно доработать, ибо сейчас он по хардкоженой ссылке срёт
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0',
+        'Accept': '*/*',
+        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Origin': 'https://www.wildberries.ru',
+        'Connection': 'keep-alive',
+        'Referer': 'https://www.wildberries.ru/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'Priority': 'u=4'
+    }
+    
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200:
+        print("Запрос успешен, статус-код:", response.status_code)
+        data = response.json()
+        with open('data.json', 'w', encoding='UTF-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+            print(f'Данные сохранены в data.json')
+    else:
+        print("Ошибка запроса, статус-код:", response.status_code)
+        pass
+
+def prepare_items(response):
+    products = []
+    
+    products_raw = response.get('data', {}).get('products')
+
 def wb_parser():
-    print("wildberries parser placeholder")
+    #print("wildberries parser placeholder")
+    category_get()
