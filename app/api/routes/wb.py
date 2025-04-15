375d88ef19cdad6a865e128c1b261f2c50b4703f
40TOHH <araslanov21@gmail.com>
2025-04-15 20:52:29 +0400

    я еблан

--- app/api/routes/wb.py
+++ app/api/routes/wb.py
@@ -69,8 +69,8 @@ async def stop_task(task_id: str):
 async def start_worker_task(task_id: str):
     if task_id in task_storage:
         raise HTTPException(status_code=400, detail="Task is already running.")
-    
-    task = asyncio.create_task(process_with_workers(SessionLocal, task_id))
+
+    task = asyncio.create_task(process_with_workers(task_id, SessionLocal))
     task_storage[task_id] = task
     logger.info(f"Task {task_id} started.")
     return {"status": f"Task {task_id} started."}
